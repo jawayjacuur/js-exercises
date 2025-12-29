9046bc77cb5ba4cf704e282b41f0e35045ece619
jawayjacuur <jawayjacuur@gnail.com>
2025-12-29 21:34:36 +0300

    added exercise4

---
+++ exercise4/main.js
@@ -0,0 +1,18 @@
+function add (a,b ) {
+    return a+b;
+}
+console.log (add(3,4))
+console.log (add(4,5))
+console.log (add(80,200))
+console.log (add(10,8))
+
+
+
+const greet = function(name){
+    console.log ("helo "   + name)
+}
+
+
+greet( "ahmed")
+greet("fatuma")
+greet("farah")
