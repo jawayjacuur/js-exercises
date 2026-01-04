13a02807ec19314410b0d6306574bfe9b9995272
jawayjacuur <jawayjacuur@gnail.com>
2026-01-04 11:35:40 +0300

    added exercise6

---
+++ exercise7/main.js
@@ -0,0 +1,16 @@
+let colors =["red","blue","green"]
+
+console.log(colors[0])
+console.log(colors[1])
+console.log(colors[2])
+
+console.log (colors)
+colors.push ("black")
+console.log (colors)
+colors.pop ()
+console.log (colors)
+colors.unshift ("yellow")
+console.log(colors)
+colors.shift ()
+console.log (colors)
+console.log(colors.length)
