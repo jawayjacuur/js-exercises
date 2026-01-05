d895efa2e498ef00afd861af09147e50ef9d45c4
jawayjacuur <jawayjacuur@gnail.com>
2026-01-05 12:46:49 +0300

    added exercise9

---
+++ exercise9/main.js
@@ -0,0 +1,20 @@
+let car = {
+    make : "tesla",
+    model :"white",
+    year :2022,
+    start : function() {
+        console.log("the car has aterted")
+    }
+     
+
+};
+
+console.log (car.make)
+console.log (car.model)
+console.log (car.year)
+
+
+
+console.log (car.start ())
+
+
