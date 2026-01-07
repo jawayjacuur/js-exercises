07b69036a9ae330b53ef7a1891c4521c8d49d2fc
jawayjacuur <jawayjacuur@gnail.com>
2026-01-07 14:12:01 +0300

    added exercise10

---
+++ exercise10/main.js
@@ -0,0 +1,15 @@
+let library = [
+    { title: "Raaxada Nolasha", 
+      author: "Abuu bakar", 
+      year: 2021 },
+
+    { title: "raali ahaanshaha rabi", 
+      author: "faysal gosaar", 
+      year: 2022 },
+    { title: "waayaheyga nolasha", 
+      author: "abdiqadir garane", 
+      year: 2020 },
+];
+
+console.log(library[0].title); 
+console.log(library[1].author); 
